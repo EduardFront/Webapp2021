9c0e5485de966494dd6b531a52f8d7f0824943b8
EduardFront <adickyago1234@gmail.com>
2021-04-29 02:25:26 +0300

    Добавьте файлы проекта.

---
+++ Webapp2021/Models/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapp2021.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+    }
+}
